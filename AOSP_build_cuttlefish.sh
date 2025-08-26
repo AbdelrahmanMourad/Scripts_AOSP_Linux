faba51c9542e49fd2a4da9ecea15487427a1c373
Mourad <AbdelrahmanMourad>
2025-08-26 06:52:31 +0300

    Updates

---
+++ AOSP_build_cuttlefish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# The shebang (#!) at the top tells the system to use the Bash shell to interpret this script.
+# It ensures your script runs with the correct shell, regardless of the user's default shell.
+# This is important for compatibility and to avoid unexpected behavior if the script uses Bash-specific features.
+# Always place the shebang as the very first line in your script.
+
+#############################################
+# Build AOSP for Cuttlefish
+#############################################
---
+++ Copy_AOSP_to_Another_Folder.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Shebang
+
+
+rsync -a --info=progress2 AOSP/ ~/WORKSPACE/AOSP_15.0.0_36/
