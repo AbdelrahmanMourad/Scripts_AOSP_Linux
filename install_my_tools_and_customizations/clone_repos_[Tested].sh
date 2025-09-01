e68f251c61bc18d0875d782624de5bcf25eea016
AbdelrahmanMourad <abdelrahmanmourad.am@gmail.com>
2025-09-01 10:25:17 +0300

    script to clone all repos

---
+++ install_my_tools_and_customizations/clone_repos_[Tested].sh
@@ -0,0 +1,41 @@
+#!bin/bash
+# Shebang
+
+# ========================================================================
+# clone my repos:
+# ========================================================================
+#
+# Make sure to create the "other" directory if it doesn't exist
+mkdir ~/WORKSPACE/REPOS/mine
+cd ~/WORKSPACE/REPOS/mine
+#
+# Clone repos:
+git clone git@github.com:AbdelrahmanMourad/Course_CPP17_DrMostafaSaadIbrahim.git
+git clone git@github.com:AbdelrahmanMourad/EmbeddedLinuxDiploma_RoboticsCorner.git
+git clone git@github.com:AbdelrahmanMourad/Course_CPP20_FromBeginnerToBeyond_Udemy.gits
+git clone git@github.com:AbdelrahmanMourad/ProblemSolving_Companies.git
+git clone git@github.com:AbdelrahmanMourad/ITI_4M_IntensiveTrainingProgram.git
+git clone git@github.com:AbdelrahmanMourad/ProblemSolving_C.git
+git clone git@github.com:AbdelrahmanMourad/Diploma_LearnInDepth_wEngKerolesShenouda.git
+git clone git@github.com:AbdelrahmanMourad/Diploma_MasterEmbeddedSystems_wEngAhmedAbdelghafar.git
+git clone git@github.com:AbdelrahmanMourad/ProblemSolving_Cpp.git
+#
+#
+
+# ========================================================================
+# clone other repos:
+# ========================================================================
+#
+# Make sure to create the "other" directory if it doesn't exist
+mkdir ~/WORKSPACE/REPOS/other
+cd ~/WORKSPACE/REPOS/other
+#
+# clone repos:
+git clone git@github.com:Mohamedsaied8/EmbeddedLinuxDiploma.git
+git clone git@github.com:AbdelrahmanMourad/cpp-cheat-sheet.git
+git clone git@github.com:AbdelrahmanMourad/CodeForces.git
+git clone git@github.com:AbdelrahmanMourad/hackerrank-solutions.git
+git clone git@github.com:AbdelrahmanMourad/Competitive-Programming.git
+git clone git@github.com:AbdelrahmanMourad/PX4-user_guide.git
+git clone git@github.com:AbdelrahmanMourad/MasteringEmbeddedSystemOnlineDiploma.git
+git clone git@github.com:AbdelrahmanMourad/-Data-structure-through-cpp-cheat-sheet.git
