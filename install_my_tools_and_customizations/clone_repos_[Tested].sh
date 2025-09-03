cc4cc273e06149ae1fd60ab94d423db1ae09bd03
Mourad <abdelrahmanmourad.am@gmail.com>
2025-09-03 06:37:15 +0300

    Clone all repos script

--- install_my_tools_and_customizations/clone_repos_[Tested].sh
+++ install_my_tools_and_customizations/clone_repos_[Tested].sh
@@ -17,16 +17,27 @@ cd ~/WORKSPACE/REPOS/mine
 echo "==================="
 echo "Cloning my repos..."
 echo "==================="
+echo
 git clone git@github.com:AbdelrahmanMourad/Scripts_AOSP_Linux.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/Course_CPP17_DrMostafaSaadIbrahim.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/EmbeddedLinuxDiploma_RoboticsCorner.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/Course_CPP20_FromBeginnerToBeyond_Udemy.gits
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/ProblemSolving_Companies.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/ITI_4M_IntensiveTrainingProgram.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/ProblemSolving_C.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/Diploma_LearnInDepth_wEngKerolesShenouda.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/Diploma_MasterEmbeddedSystems_wEngAhmedAbdelghafar.git
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/ProblemSolving_Cpp.git
+echo "--------------------------------------"
 # 
 # 
 # 
@@ -40,17 +51,35 @@ mkdir ~/WORKSPACE/REPOS/other
 cd ~/WORKSPACE/REPOS/other
 #
 # clone repos:
+echo
 echo "======================"
 echo "Cloning other repos..."
 echo "======================"
+echo
+echo "-------------------"
 git clone git@github.com:Mohamedsaied8/EmbeddedLinuxDiploma.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/cpp-cheat-sheet.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/CodeForces.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/hackerrank-solutions.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/Competitive-Programming.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/PX4-user_guide.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/MasteringEmbeddedSystemOnlineDiploma.git
+echo "--------------------------------------"
+echo "-------------------"
 git clone git@github.com:AbdelrahmanMourad/-Data-structure-through-cpp-cheat-sheet.git
+echo "--------------------------------------"
 # 
 # 
 # 
