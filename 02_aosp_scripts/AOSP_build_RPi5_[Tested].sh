3ef4098ad897ed6276f6f317c0a315dc79d4d604
AbdelrahmanMourad <abdelrahmanmourad.am@gmail.com>
2025-09-10 14:02:09 +0300

    Update

--- 02_aosp_scripts/AOSP_build_RPi5_[Tested].sh
+++ 02_aosp_scripts/AOSP_build_RPi5_[Tested].sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 # Shebang
 
-
-
 # ==================================================================
 # <-- Use your existing source directory here -->
 #   (Must be the same as where you ran `repo sync` to download AOSP source)
 # ==================================================================
 cd ~/WORKSPACE/AOSP_15.0.0_36
 
-
 # ==================================================================
 # Fix: (workaround to allow unpriviliged user namespace through Apparmor) 
 # ==================================================================
 sudo sysctl -w kernel.apparmor_restrict_unprivileged_unconfined=0 
 sudo sysctl -w kernel.apparmor_restrict_unprivileged_userns=0 
-
 
 # =================================================================
 # Hardware List:
@@ -56,11 +52,9 @@ sudo apt-get install git
 git config --global user.name "AbdelrahmanMourad"
 git config --global user.email "abdelrahmanmourad.am@gmail.com"
 
-
 # =================================================================
 # Download Android 15 AOSP Source for Raspberry Pi 5
-# =================================================================
-# 
+# ================================================================= 
 # 
 # Install the required packages on the Build Machine
 sudo apt-get install bc coreutils dosfstools e2fsprogs fdisk kpartx mtools ninja-build pkg-config python3-pip
@@ -72,7 +66,7 @@ python3 -m venv ~/aosp-venv
 pip install --user meson mako jinja2 ply pyyaml
 
 
-#
+
 # ----------------------------
 # Initialize the android repo:
 # ---------------------------
@@ -82,12 +76,13 @@ pip install --user meson mako jinja2 ply pyyaml
 # # raspberrypi-vanilla
 repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r36
 curl -o .repo/local_manifests/manifest_brcm_rpi.xml -L https://raw.githubusercontent.com/raspberry-vanilla/android_local_manifest/android-15.0/manifest_brcm_rpi.xml --create-dirs
-# #
+#
 # # Or optionally, you can reduce download size by creating a shallow clone and removing unneeded projects
 # repo init -u https://android.googlesource.com/platform/manifest -b android-15.0.0_r36 --depth=1
 # curl -o .repo/local_manifests/manifest_brcm_rpi.xml -L https://raw.githubusercontent.com/raspberry-vanilla/android_local_manifest/android-15.0/manifest_brcm_rpi.xml --create-dirs
 # curl -o .repo/local_manifests/remove_projects.xml -L https://raw.githubusercontent.com/raspberry-vanilla/android_local_manifest/android-15.0/remove_projects.xml
-# #
+
+
 # ------------------------------
 # Repo Sync once Repo Init done:
 # ------------------------------
@@ -95,29 +90,21 @@ curl -o .repo/local_manifests/manifest_brcm_rpi.xml -L https://raw.githubusercon
 # repo sync
 #
 # ==================================================================
-# Fix: (Clean the old builds workaround) 
-# ==================================================================
-make clean
-#
-# ==================================================================
 # Fix: (Repo sync issues workaround) 
 # ==================================================================
 repo sync -c -j$(nproc --all) --force-sync
 # repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
-
-
 # =================================================================
 # Builf AOSP Source for Raspberry Pi 5
 # =================================================================
-#
 #
 # --------------------------------
 # Setup Android build environment:
 # --------------------------------
 source build/envsetup.sh
 
-#
+
 # ==================================================================
 # Fix: (Clean the old builds workaround) 
 # ==================================================================
@@ -176,7 +163,6 @@ lunch aosp_rpi5_car-trunk_staging-userdebug
 m
 # Once system images are generated they will be stored at “ out/target/product/rpi5/”.
 
-
 # # ==================================================================
 # make bootimage systemimage vendorimage -j$(nproc)
 # what is the difference between this line and the command : "m"
@@ -202,9 +188,6 @@ m
 # Tip:
 # For most Android builds, using m is recommended for convenience and reliability.
 # # ==================================================================
-
-
-
 
 
 # # Search for RPi5 related files in device/ directory
